3d596a1c39a0af285a3ea92873c1dd8eddfa0963
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-17 16:40:23 +0300

    i3: add missing dependencies

--- packages-x11/i3/build.sh
+++ packages-x11/i3/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_VERSION=4.15
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=217d524d1fbc85ae346b25f6848d1b7bcd2c23184ec88d29114bf5a621385326
-TERMUX_PKG_DEPENDS="libandroid-glob, libev, libposix-shm, libxkbcommon, startup-notification, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
+TERMUX_PKG_DEPENDS="libandroid-glob, libcairo-x, libev, libposix-shm, libxkbcommon, pango, pcre, startup-notification, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
 
 termux_step_pre_configure() {
 	export LIBS="-landroid-glob -lposix-shm -llog"
