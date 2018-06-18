45b7bef12b752462f4fefe9edc88095617697f30
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-18 15:45:29 +0300

    new package: libjemalloc

---
+++ packages/libjemalloc/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://www.canonware.com/jemalloc/
+TERMUX_PKG_DESCRIPTION="General-purpose scalable concurrent malloc implementation"
+TERMUX_PKG_VERSION=5.0.1
+TERMUX_PKG_SRCURL=https://github.com/jemalloc/jemalloc/releases/download/${TERMUX_PKG_VERSION}/jemalloc-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=4814781d395b0ef093b21a08e8e6e0bd3dab8762f9935bbfb71679b0dea7c3e9
+TERMUX_PKG_BUILD_IN_SRC=true
