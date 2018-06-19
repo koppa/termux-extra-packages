fc7d468c0f73425c3adced9d051d98130c6cc60d
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-20 01:02:18 +0300

    xorg-mkfontdir: this package is platform-independent

--- packages-x11/xorg-mkfontdir/build.sh
+++ packages-x11/xorg-mkfontdir/build.sh
@@ -5,4 +5,5 @@ TERMUX_PKG_DESCRIPTION="Create an index of scalable font files for X"
 TERMUX_PKG_VERSION=1.0.7
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/app/mkfontdir-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=56d52a482df130484e51fd066d1b6eda7c2c02ddbc91fe6e2be1b9c4e7306530
-TERMUX_PKG_DEPENDS="bash, xorg-mkfontscale"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_DEPENDS="dash, xorg-mkfontscale"
