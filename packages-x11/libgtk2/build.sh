2ee3c65a15deb7cdde50a3f9341b7c16cc40ed04
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-23 20:22:49 +0300

    libgtk (2,3): fix typo in build.sh

--- packages-x11/libgtk2/build.sh
+++ packages-x11/libgtk2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=http://www.gtk.org/
 TERMUX_PKG_DESCRIPTION="GObject-based multi-platform GUI toolkit (legacy)"
 TERMUX_PKG_VERSION=2.24.32
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/GNOME/gtk/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=961678c64ad986029befd7bdd8ed3e3849e2c5e54d24affbc7d49758245c87fa
 TERMUX_PKG_DEPENDS="adwaita-icon-theme, coreutils, desktop-file-utils, gtk-update-icon-cache, libatk, libcairo-x, librsvg, libxcomposite, libxcursor, libxdamage, libxi, libxinerama, libxrandr, pango-x, shared-mime-info"
@@ -19,7 +20,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 ## provided by libgtk3's sub package
-TERMUX_PKG_RM="bin/gtk-update-icon-cache"
+TERMUX_PKG_RM_AFTER_INSTALL="bin/gtk-update-icon-cache"
 
 termux_step_pre_configure() {
     NOCONFIGURE=1 ./autogen.sh
