dd7291966d2a088a62784c4c5484df04a0ef941e
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-24 02:04:27 +0300

    qemu-system: replace getdtablesize() with sysconf(_SC_OPEN_MAX) instead of hardcoded value

--- packages-x11/qemu-system/build.sh
+++ packages-x11/qemu-system/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.qemu.org
 TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer"
 TERMUX_PKG_VERSION=2.11.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8a5145d1f8bd2eadc6776f3e13c68cd28d01349e30639bdbcb26ac588d668686
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libbz2, libcurl, libgnutls, libjpeg-turbo, liblzo, libnettle, libpixman, libpng, libsdl, libseccomp, libssh2, libutil, ncurses"
@@ -54,17 +54,17 @@ termux_step_post_massage()
 {
     (
         ## by default, alias 'qemu' will be a qemu-system-x86_64
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin || exit 1
+        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin
         ln -sf qemu-system-x86_64 qemu
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man/man1 || exit 1
+        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man/man1
         sed -i 's/qemu\\-system\\-i386/qemu\\-system\\-x86_64/g' qemu.1
 
         ## gzip manpages
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man || exit 1
+        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man
         gzip -9 $(find . -type f)
 
         ## symlink manpages
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man/man1 || exit 1
+        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man/man1
         ln -sf qemu.1.gz qemu-system-aarch64.1.gz
         ln -sf qemu.1.gz qemu-system-arm.1.gz
         ln -sf qemu.1.gz qemu-system-i386.1.gz
