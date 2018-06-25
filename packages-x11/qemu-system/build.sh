4e48ee1eb0d23ddf7f4696f5cce8d134b9d18548
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-25 15:05:52 +0300

    qemu: update to v2.12.0, enable linux-user CPU emulation mode, split packages

--- packages-x11/qemu-system/build.sh
+++ packages-x11/qemu-system/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://www.qemu.org
-TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer"
-TERMUX_PKG_VERSION=2.11.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (i386, x86_64 targets)"
+TERMUX_PKG_VERSION=2.12.0
 TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8a5145d1f8bd2eadc6776f3e13c68cd28d01349e30639bdbcb26ac588d668686
-TERMUX_PKG_DEPENDS="glib, libandroid-support, libbz2, libcurl, libgnutls, libjpeg-turbo, liblzo, libnettle, libpixman, libpng, libsdl, libseccomp, libssh2, libutil, ncurses"
+TERMUX_PKG_SHA256=e69301f361ff65bf5dabd8a19196aeaa5613c1b5ae1678f0823bdf50e7d5c6fc
+TERMUX_PKG_DEPENDS="glib, libandroid-shmem, libandroid-support, libbz2, libcurl, libgnutls, libjpeg-turbo, liblzo, libnettle, libpixman, libpng, libsasl, libsdl, libseccomp, libssh2, libutil, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/qemu-nbd
@@ -23,7 +22,7 @@ termux_step_configure()
                 --objcc="${CC}" \
                 --extra-cflags="${CFLAGS}" \
                 --extra-cxxflags="${CXXFLAGS}" \
-                --extra-ldflags="${LDFLAGS} -llog" \
+                --extra-ldflags="${LDFLAGS} -landroid-shmem -llog" \
                 --smbd="${TERMUX_PREFIX}/bin/smbd" \
                 --disable-guest-agent \
                 --enable-pie \
@@ -33,6 +32,7 @@ termux_step_configure()
                 --enable-vnc \
                 --enable-vnc-jpeg \
                 --enable-vnc-png \
+                --enable-vnc-sasl \
                 --disable-mpath \
                 --disable-xen \
                 --enable-curl \
@@ -50,24 +50,15 @@ termux_step_configure()
                 --target-list=aarch64-softmmu,arm-softmmu,i386-softmmu,x86_64-softmmu
 }
 
-termux_step_post_massage()
+termux_step_post_make_install()
 {
-    (
-        ## by default, alias 'qemu' will be a qemu-system-x86_64
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin
-        ln -sf qemu-system-x86_64 qemu
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man/man1
-        sed -i 's/qemu\\-system\\-i386/qemu\\-system\\-x86_64/g' qemu.1
+    ## by default, alias 'qemu' will be a qemu-system-x86_64
+    ln -sfr "${TERMUX_PREFIX}/bin/qemu-system-x86_64" "${TERMUX_PREFIX}/bin/qemu"
+    sed -i 's/qemu\\-system\\-i386/qemu\\-system\\-x86_64/g' "${TERMUX_PREFIX}/share/man/man1/qemu.1"
 
-        ## gzip manpages
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man
-        gzip -9 $(find . -type f)
-
-        ## symlink manpages
-        cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/share/man/man1
-        ln -sf qemu.1.gz qemu-system-aarch64.1.gz
-        ln -sf qemu.1.gz qemu-system-arm.1.gz
-        ln -sf qemu.1.gz qemu-system-i386.1.gz
-        ln -sf qemu.1.gz qemu-system-x86_64.1.gz
-    )
+    ## symlink manpages
+    ln -sfr "${TERMUX_PREFIX}/share/man/man1/qemu.1" "${TERMUX_PREFIX}/share/man/man1/qemu-system-aarch64.1"
+    ln -sfr "${TERMUX_PREFIX}/share/man/man1/qemu.1" "${TERMUX_PREFIX}/share/man/man1/qemu-system-arm.1"
+    ln -sfr "${TERMUX_PREFIX}/share/man/man1/qemu.1" "${TERMUX_PREFIX}/share/man/man1/qemu-system-i386.1"
+    ln -sfr "${TERMUX_PREFIX}/share/man/man1/qemu.1" "${TERMUX_PREFIX}/share/man/man1/qemu-system-x86_64.1"
 }
---
+++ packages/qemu-system-headless/qemu-user-aarch64.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="bin/qemu-aarch64"
+TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libandroid-support, glib, libc++"
+TERMUX_SUBPKG_DESCRIPTION="AArch64 CPU usermode emulation"
