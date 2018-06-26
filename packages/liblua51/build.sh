3ff0fdc013fdaaa9d41bfe0cc5ee2f56d58386aa
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-26 16:17:54 +0300

    liblua51: fix patch files, fix manpage names, fix installation of pkg-config file

--- packages/liblua51/build.sh
+++ packages/liblua51/build.sh
@@ -3,44 +3,32 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.lua.org
 TERMUX_PKG_DESCRIPTION="Shared library for the Lua interpreter (v5.1.x)"
 TERMUX_PKG_VERSION=5.1.5
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=http://www.lua.org/ftp/lua-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2640fc56a795f29d28ef15e13c34a47e223960b0240e8cb0a82d9b0738695333
 TERMUX_PKG_BUILD_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
-termux_step_configure()
-{
-    sed -e 's:llua:llua5.1:' -e 's:/include:/include/lua5.1:' -i etc/lua.pc
-    sed -r -e '/^LUA_(SO|A|T)=/ s/lua/lua5.1/' -e '/^LUAC_T=/ s/luac/luac5.1/' -i src/Makefile
+termux_step_make() {
+    make -j ${TERMUX_MAKE_PROCESSES} \
+            MYCFLAGS="${CFLAGS}" \
+            MYLDFLAGS="${LDFLAGS}" \
+            CC="${CC}" \
+            CXX="${CXX}" \
+            linux
 }
 
-termux_step_make()
-{
-    make -j4 MYCFLAGS="${CFLAGS}" MYLDFLAGS="${LDFLAGS}" CC="${CC}" CXX="${CXX}" linux
-}
+termux_step_make_install() {
+    make \
+        TO_BIN='lua5.1 luac5.1' \
+        TO_LIB="liblua5.1.so liblua5.1.so.5.1 liblua5.1.so.${TERMUX_PKG_VERSION}" \
+        INSTALL_DATA='cp -d' \
+        INSTALL_TOP="${TERMUX_PREFIX}" \
+        INSTALL_INC="${TERMUX_PREFIX}/include/lua5.1" \
+        INSTALL_MAN="${TERMUX_PREFIX}/share/man/man1" \
+        install
+    install -Dm644 etc/lua.pc "${TERMUX_PREFIX}/lib/pkgconfig/lua51.pc"
 
-termux_step_make_install()
-{
-    (
-        cd "${TERMUX_PKG_SRCDIR}"
-        make \
-            TO_BIN='lua5.1 luac5.1' \
-            TO_LIB="liblua5.1.a liblua5.1.so liblua5.1.so.5.1 liblua5.1.so.${TERMUX_PKG_VERSION}" \
-            INSTALL_DATA='cp -d' \
-            INSTALL_TOP="${TERMUX_PREFIX}" \
-            INSTALL_INC="${TERMUX_PREFIX}/include/lua5.1" \
-            INSTALL_MAN="${TERMUX_PREFIX}/share/man/man1" \
-            install
-
-        if [ -z "${TERMUX_DEBUG}" ]; then
-            ${STRIP} -s "${TERMUX_PREFIX}/bin/lua5.1"
-            ${STRIP} -s "${TERMUX_PREFIX}/bin/luac5.1"
-            ${STRIP} --strip-unneeded "${TERMUX_PREFIX}/lib/liblua5.1.so.${TERMUX_PKG_VERSION}"
-        fi
-
-        ${TERMUX_ELF_CLEANER} "${TERMUX_PREFIX}/bin/lua5.1"
-        ${TERMUX_ELF_CLEANER} "${TERMUX_PREFIX}/bin/luac5.1"
-        ${TERMUX_ELF_CLEANER} "${TERMUX_PREFIX}/lib/liblua5.1.so.${TERMUX_PKG_VERSION}"
-    )
+    mv -f "${TERMUX_PREFIX}/share/man/man1/lua.1" "${TERMUX_PREFIX}/share/man/man1/lua5.1.1"
+    mv -f "${TERMUX_PREFIX}/share/man/man1/luac.1" "${TERMUX_PREFIX}/share/man/man1/luac5.1.1"
 }
