edb203ef4d2ad927093d2a9d4ab4f7fbacfe5ab9
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-29 11:59:56 +0300

    riscvemu: update source urls and sha-256

--- packages/riscvemu/build.sh
+++ packages/riscvemu/build.sh
@@ -2,10 +2,13 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://bellard.org/riscvemu/
 TERMUX_PKG_DESCRIPTION="RISC-V system emulator"
+
+_COMMIT=d81d3be5b15ae8506668bace1cde82cf0e04a26e
 TERMUX_PKG_VERSION=20170806
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/xeffyr/riscvemu/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ab78b040479a92fa6a28e098c9109d3be3049ab4a75070e4363ddcd22155b96a
+
+TERMUX_PKG_SRCURL=https://github.com/xeffyr/riscvemu/archive/${_COMMIT}.tar.gz
+TERMUX_PKG_SHA256=a1c806f8436fb82fa49accc337263bf65292f7d92edd8ab94624c3e816502a87
 TERMUX_PKG_DEPENDS="libcurl, openssl"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
