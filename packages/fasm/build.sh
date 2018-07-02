0799b0da91e2ae4c865807c9031b863f2f654de9
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-07-02 23:21:11 +0300

    fasm: fix typo in wrapper script

--- packages/fasm/build.sh
+++ packages/fasm/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://flatassembler.net/
 TERMUX_PKG_DESCRIPTION="The Flat Assembler. Runs on Android via QEMU."
 TERMUX_PKG_VERSION=1.73.04
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://flatassembler.net/fasm-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_SHA256=4145d0341e916165c22c7b5ab62ed511ddd0aee4c1c406e97ca5b2ef929b1d78
 TERMUX_PKG_DEPENDS="dash, qemu-user-i386"
