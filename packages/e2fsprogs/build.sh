741073484543d85b01e6525b6f2b93d4b200313d
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-07-08 22:59:21 +0300

    e2fsprogs: fix issues with libblkid and install config file for mke2fs

--- packages/e2fsprogs/build.sh
+++ packages/e2fsprogs/build.sh
@@ -3,10 +3,13 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=http://e2fsprogs.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Ext2/3/4 filesystem utilities"
 TERMUX_PKG_VERSION=1.44.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v${TERMUX_PKG_VERSION}/e2fsprogs-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8324cf0b6e81805a741d94087b00e99f7e16144f1ee5a413709a1fa6948b126c
 TERMUX_PKG_CONFFILES="etc/mke2fs.conf"
-TERMUX_PKG_DEPENDS="libuuid"
+
+## util-linux provides libblkid
+TERMUX_PKG_DEPENDS="libuuid, util-linux"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --sbindir=${TERMUX_PREFIX}/bin
@@ -15,5 +18,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-defrag
 --disable-e2initrd-helper
 --disable-libuuid
+--disable-libblkid
 --disable-uuidd
 "
+
+termux_step_post_make_install() {
+    install -Dm644 "${TERMUX_PKG_SRCDIR}/misc/mke2fs.conf.in" "${TERMUX_PREFIX}/etc/mke2fs.conf"
+}
