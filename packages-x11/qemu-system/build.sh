6778430d6e05ac6ed438c3143809bb3006595adc
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-08-02 23:55:50 +0300

    qemu-system: don't build with libxml2

--- packages-x11/qemu-system/build.sh
+++ packages-x11/qemu-system/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.qemu.org
 TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (i386, x86_64 targets)"
 TERMUX_PKG_VERSION=2.12.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e69301f361ff65bf5dabd8a19196aeaa5613c1b5ae1678f0823bdf50e7d5c6fc
 TERMUX_PKG_DEPENDS="glib, libandroid-shmem, libandroid-support, libbz2, libc++, libcurl, libgnutls, libjpeg-turbo, liblzo, libnettle, libpixman, libpng, libsasl, libsdl, libseccomp, libssh2, libutil, ncurses"
@@ -48,6 +48,7 @@ termux_step_configure()
                 --enable-tpm \
                 --enable-libssh2 \
                 --disable-jemalloc \
+                --disable-libxml2 \
                 --target-list=aarch64-softmmu,arm-softmmu,i386-softmmu,x86_64-softmmu
 }
 
